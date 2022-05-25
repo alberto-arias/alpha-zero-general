f55d3696e3f88e5bd60a2937767603724b19ebb3
parazoa <alberto.ariasdrake@gmail.com>
2022-05-25 16:47:39 +0200

    Removed verbose message with prediction time

--- duffer/NNet.py
+++ duffer/NNet.py
@@ -65,8 +65,6 @@ class NNetWrapper(NeuralNet):
 
         # run
         pi, v = self.nnet.model.predict(board, verbose=0)
-
-        print('PREDICTION TIME TAKEN : {0:03f}'.format(time.time()-start))
         return pi[0], v[0]
 
     def save_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
